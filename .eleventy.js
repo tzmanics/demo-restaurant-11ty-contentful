c2a226a358afb8f7460ea0e4828d6528cabf5f3f
tzmanics <tzmanics@gmail.com>
2021-09-27 23:15:36 -0400

    adds menu work

--- .eleventy.js
+++ .eleventy.js
@@ -4,20 +4,6 @@ const fs = require('fs-extra');
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ 'src/css': 'css' });
 
-  // Compile Sass before a build
-  eleventyConfig.on('beforeBuild', () => {
-    let result = sass.renderSync({
-      file: 'src/scss/main.scss',
-      sourceMap: false,
-      outputStyle: 'compressed',
-    });
-    fs.ensureDirSync('dist/css/');
-    fs.writeFile('dist/css/main.css', result.css, (err) => {
-      if (err) throw err;
-      console.log('CSS generated');
-    });
-  });
-
   // where do things live?
   return {
     dir: {
