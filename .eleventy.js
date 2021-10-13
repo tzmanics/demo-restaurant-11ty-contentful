ef4b6e6a0c483393d7d13546a295a0fc56f8f55a
tzmanics <tzmanics@gmail.com>
2021-10-13 10:49:27 -0400

    adds content, styling, markdown support

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 const sass = require('sass');
 const fs = require('fs-extra');
+const marked = require('marked');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ 'src/css': 'css' });
+
+  // Markdown filter
+  eleventyConfig.addFilter('marked', (str) => marked(str));
 
   // where do things live?
   return {
