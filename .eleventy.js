8490393b33984beb373cf08587500df581ef95c6
Tara Z. Manicsic <tzmanics@gmail.com>
2021-10-26 23:15:25 -0400

    adds date formatting support

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const sass = require('sass');
 const fs = require('fs-extra');
 const marked = require('marked');
+const { DateTime } = require('luxon');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({
@@ -10,6 +11,10 @@ module.exports = (eleventyConfig) => {
 
   // Markdown filter
   eleventyConfig.addFilter('marked', (str) => marked(str));
+  // Date filter
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   // where do things live?
   return {
